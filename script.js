667377546f1709e4117d932151a7b0b13b6d1305
Emily Pondaven <83513279+emilypondaven@users.noreply.github.com>
2024-01-24 03:36:55 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var correctUsername = "emily";
+    var correctPassword = "emily123"
+
+    if(username==correctUsername && password==correctPassword) {
+        window.location.replace("mainMenu.html")
+        return false;
+    }
+    else {
+        alert("Login failed");
+    }
+}
